5a316d2167ed0ee0989bff8afebbf29fd47e9501
syhx <u202114724@hust.edu.cn>
2023-11-02 16:22:41 +0800

    remove some useless data and add discover-page

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  webpack: (config) => {
-    config.resolve.alias["@assets"] = path.join(__dirname, "assets");
-    return config;
-  },
 };
 
 module.exports = nextConfig;
--- pages/discover/index.js
+++ pages/discover/index.js
@@ -1,8 +1,22 @@
-import React from "react";
-export default function Discover() {
+import React, { memo } from "react";
+import Router from "next/router";
+import Layout1 from "@/components/Layout1";
+import { DiscoverWrapper } from "./style";
+import { discoverMenu } from "@/data/local-data.js";
+import Link from "next/link";
+export default memo(function Discover() {
+  let a = 11;
   return (
-    <>
-      <h1>Discover</h1>
-    </>
+    <DiscoverWrapper>
+      <div className="top">
+        {discoverMenu.map((item) => {
+          return (
+            <div className="title" key={item.title}>
+              <Link href={item.link}>{item.title}</Link>
+            </div>
+          );
+        })}
+      </div>
+    </DiscoverWrapper>
   );
-}
+});
