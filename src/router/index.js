b7a2ae68df021fad3c375a2ca1cff719fb0bbc53
syhx <u202114724@hust.edu.cn>
2023-11-02 14:02:54 +0800

    add some component file

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Redirect } from "next";
+
+const HYDiscover = React.lazy(() =>
+  import("@/../pages/discover/c-pages/recommend")
+);
+const HYRanking = React.lazy(() =>
+  import("@/../pages/discover/c-pages/ranking")
+);
+export default AA[
+  ({
+    path: "/",
+    exact: true,
+    render: () => <Redirect to={"/discover"} />,
+  },
+  {
+    path: "/discover",
+    component: HYDiscover,
+    routes: [
+      {
+        path: "/discover",
+        exact: true,
+        render: () => <Redirect />,
+      },
+      {
+        path: "/discover/recommend",
+        component: HYDiscover,
+      },
+      {
+        path: "/discover/ranking",
+        component: HYRanking,
+      },
+    ],
+  })
+];
+
