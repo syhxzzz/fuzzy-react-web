1546be8cc19a00adb949bdeb9798a925bc5f2802
syhx <u202114724@hust.edu.cn>
2023-11-07 12:31:47 +0800

    little modification

--- pages/discover/recommend/component/recommend-hot-recommend/index.js
+++ pages/discover/recommend/component/recommend-hot-recommend/index.js
@@ -1,13 +1,26 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useEffect } from "react";
 import { RecommendWrapper } from "./style";
 import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import { getRecommend } from "../../store/actionCreators";
 // import useHistory from
 export default memo(function HotRecommend() {
+  const router = useRouter();
   const state = useSelector((state1) => ({
     recommends: state1.recommend.get("hotRecommends"),
   }));
   const dispatch = useDispatch();
-  // TODO()
-  // const history = useHistory();
+
+  useEffect(() => {
+    dispatch(getRecommend());
+  }, [dispatch]);
+
+  const keywordClick = useCallback(
+    (keyword) => {
+      router.push({ url: "/discover/songs", cat: keyword });
+    },
+    [router]
+  );
+
   return <RecommendWrapper></RecommendWrapper>;
 });
