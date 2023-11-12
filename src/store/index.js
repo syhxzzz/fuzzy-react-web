337c801c1dba426fb847a82727c6a91357547d9b
syhx <u202114724@hust.edu.cn>
2023-11-12 21:21:03 +0800

    change CreateStore to configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,6 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import reducer from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
-const composeEnhancers = compose;
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = configureStore({ reducer: reducer });
 
 export default store;
