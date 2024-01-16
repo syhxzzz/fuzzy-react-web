293efb8246293765251b5c767ae1dc391cc3b129
syhx <u202114724@hust.edu.cn>
2024-01-16 17:22:29 +0800

    finish playlist item

--- src/components/play-bar/component/play-panel/index.js
+++ src/components/play-bar/component/play-panel/index.js
@@ -12,18 +12,41 @@ export const PlayPanel = () => {
         <h4>
           播放列表<span className="">({state.songList.length})</span>
         </h4>
-        <a className="addAll">收藏全部</a>
+        <a className="addAll sprite_playlist">
+          <span className="sprite_playlist"></span>收藏全部
+        </a>
         <span className="line"></span>
-        <a className="clear">清除</a>
+        <a className="clear">
+          <span className="sprite_playlist"></span>清除
+        </a>
         <p className="lyrics">{state.currentSong.name}</p>
         <span className="close sprite_playlist">关闭</span>
       </div>
       <div className="body">
-        <div className="list-content">
+        <ul className="list-content">
           {state.songList.map((item) => {
-            return <div key={item.id}>{item.name}</div>;
+            return (
+              <li key={item.id} className="item">
+                <div className="left-content">
+                  <div className="play-btn sprite_playlist"></div>
+                  <div className="name">{item.name}</div>
+                </div>
+                <div className="right-content">
+                  <div className="operate">
+                    <a title="收藏" className="collect sprite_playlist"></a>
+                    <a title="分享" className="share sprite_playlist"></a>
+                    <a title="下载" className="download sprite_playlist"></a>
+                    <a title="删除列表" className="delete sprite_playlist"></a>
+                  </div>
+                  <div className="artist">疏影横斜</div>
+                  <div className="duration">4:15</div>
+                  <div className="song-info"></div>
+                </div>
+              </li>
+            );
           })}
-        </div>
+        </ul>
+        <span className="divider"></span>
         <div className="lyrics"></div>
       </div>
     </PlayPanelWrapper>
--- src/components/play-bar/component/play-panel/style.js
+++ src/components/play-bar/component/play-panel/style.js
@@ -10,7 +10,8 @@ export const PlayPanelWrapper = styled.div`
   background-color: black;
   display: flex;
   flex-direction: column;
-
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
   color: white;
   .head {
     width: 100%;
@@ -25,17 +26,25 @@ export const PlayPanelWrapper = styled.div`
       color: #e2e2e2;
     }
     .addAll {
+      display: flex;
       left: 398px;
       position: absolute;
       top: 12px;
       height: 15px;
       line-height: 15px;
       cursor: pointer;
+      span {
+        display: inline-block;
+        margin: 1px 6px 0 0;
+        width: 16px;
+        background-position: -24px 0;
+        height: 16px;
+      }
     }
     .line {
       left: 477px;
       position: absolute;
-      top: 13px;
+      top: 16px;
       height: 15px;
       border-left: 1px solid #000;
       border-right: 1px solid #2c2c2c;
@@ -51,6 +60,23 @@ export const PlayPanelWrapper = styled.div`
       font-size: 14px;
       text-align: center;
     }
+
+    .clear {
+      display: flex;
+      left: 490px;
+      top: 12px;
+      height: 15px;
+      line-height: 15px;
+      position: absolute;
+      span {
+        display: inline-block;
+        height: 16px;
+        width: 16px;
+        background-position: -51px 0;
+        margin: 1px 6px 0 0;
+      }
+    }
+
     .close {
       position: absolute;
       top: 6px;
@@ -63,6 +89,86 @@ export const PlayPanelWrapper = styled.div`
 
       overflow: hidden;
       text-indent: -999px;
+    }
+  }
+  .body {
+    display: flex;
+    flex-grow: 1;
+    .list-content {
+      width: 553px;
+      .item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 28px;
+        .left-content {
+          display: flex;
+          height: 100%;
+          align-items: center;
+          .play-btn {
+            margin-left: 10px;
+            background-position: -182px 0;
+            width: 10px;
+            height: 13px;
+          }
+          .name {
+            margin-left: 10px;
+            width: 256px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+        }
+        .right-content {
+          display: flex;
+          .operate {
+            margin-left: 10px;
+            display: flex;
+            width: 94px;
+            justify-content: space-between;
+            .collect {
+              width: 16px;
+              height: 16px;
+              background-position: -24px 0;
+            }
+            .share {
+              width: 16px;
+              height: 16px;
+              background-position: 1px 0;
+            }
+            .download {
+              width: 16px;
+              height: 16px;
+              background-position: -57px -50px;
+            }
+            .delete {
+              width: 16px;
+              height: 16px;
+              background-position: -51px 1px;
+            }
+          }
+          .artist {
+            margin-left: 10px;
+            width: 70px;
+          }
+          .duration {
+            margin-left: 10px;
+            width: 35px;
+          }
+          .song-info {
+            margin-left: 10px;
+            width: 33px;
+          }
+        }
+      }
+    }
+    .divider {
+      width: 5px;
+      height: 100%;
+      background-color: white;
+    }
+    .lyrics {
+      background-color: red;
     }
   }
 `;
